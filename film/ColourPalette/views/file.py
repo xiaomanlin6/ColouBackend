88e9a59d62125b04ecc3bd51913ce63b9e4412ad
Xiaoman Lin <xiaomanlin6@gmail.com>
2023-12-01 11:45:35 -0500

    default avatar

--- film/ColourPalette/views/file.py
+++ film/ColourPalette/views/file.py
@@ -1,6 +1,9 @@
 from django.views import View
 from django.http import JsonResponse
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.conf import settings
+from django.core.files import File
+
 class AvatarView(View):
     def post(self, request):
         res = {
@@ -8,10 +11,17 @@ class AvatarView(View):
             'msg': 'upload error'
         }
         file: InMemoryUploadedFile = request.FILES.get('file')
-        request.user.avatar = file
-        request.user.save()
-        res['code'] = 200
-        res['msg'] = 'upload success'
+        if file:
+            request.user.avatar = file
+            request.user.save()
+            res['code'] = 200
+            res['msg'] = 'upload success'
+        else:
+            # Set default avatar
+            default_avatar_path = settings.MEDIA_ROOT + '/default_avatar.jpg'
+            with open(default_avatar_path, 'rb') as default_avatar_file:
+                request.user.avatar.save('default_avatar.jpg', File(default_avatar_file), save=True)
+            res['code'] = 200
+            res['msg'] = 'default avatar set'
+
         return JsonResponse(res)
-
-
